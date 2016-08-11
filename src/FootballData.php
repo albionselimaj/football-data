a3dd7476b7aad89dc56594138fcd0759862e1e5a
= <albion.selimaj.4@gmail.com>
2016-08-11 14:55:04 +0200

    Test

--- src/FootballData.php
+++ src/FootballData.php
@@ -6,67 +6,65 @@ use GuzzleHttp\Client;
 class FootballData
 {
 	protected $client;
-	
+
 	public function __construct(Client $client )
-	{	
+	{
 		$this->client = $client;
-
-
 	}
-	
+
 	public function getLeagues( $v="v1")
 	 	{
 		$leagues = $this->client->get("{$v}/soccerseasons")->json();
-		
+
 		return $leagues;
 	}
-	
+
 	public function getLeagueFixtures($id, $v="v1")
-	{		
+	{
 		$fixtures = $this->client->get("{$v}/soccerseasons/{$id}/fixtures")->json();
-		
+
 		return $fixtures;
 	}
-	
+
 	public function getFixtures($id, $matchday, $v="v1")
 	{
 		$fixtures = $this->client->get("{$v}/soccerseasons/{$id}/fixtures/?matchday={$matchday}")->json();
-		
+
 		return $fixtures;
 	}
-	
+
 	public function getLeagueTable($id, $v="v1")
 	{
 		$league = $this->client->get("{$v}/soccerseasons/{$id}/leagueTable")->json();
-		
-		return $league;		
+
+		return $league;
 	}
-	
+
 	public function getLeagueTeams($id, $v="v1")
 	{
 		$teams = $this->client->get("{$v}/soccerseasons/{$id}/teams")->json();
-		
-		return $teams;		
+
+		return $teams;
 	}
-	
+
 	public function getTeam($id, $v="v1")
 	{
 		$team = $this->client->get("{$v}/teams/". $id)->json();
 
-		return $team;		
+		return $team;
 	}
-	
+
 	public function getTeamPlayers($id, $v="v1")
 	{
 		$players = $this->client->get("{$v}/teams/". $id ."/players")->json();
-		
+
 		return $players;
 	}
-	
+
 	public function getTeamFixtures($id, $v="v1")
 	{
 		$team_fixtures = $this->client->get("{$v}/teams/". $id ."/fixtures")->json();
-		
+
 		return $team_fixtures;
 	}
 
@@ -74,7 +72,7 @@ class FootballData
 	public function getFixture($id, $v="v1")
 	{
 		//$fixture = $this->client->get( "{$v}/fixtures/". $id. "/head2head=".$matches)->json();
-		
+
 		$fixture = $this->client->get( "{$v}/fixtures/". $id)->json();
 
 		return $fixture;
